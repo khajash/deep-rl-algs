b58ca1136139d0a27f4e3aa8a20105dcf5c67921
Kate <kshajash@gmail.com>
2022-08-09 23:21:41 -0700

    Fix random seed

--- drllib/test_env.py
+++ drllib/test_env.py
@@ -20,7 +20,14 @@ def main(config_file):
     # env = gym.make(config['env'], **config['env_params'])
     # env = wrap_deepmind(en)v
     # try wrappers RecordVideo or RecordEpisodeStatistics to document episode stats
+    to_render = True
+    seed = 17
 
+
+    import numpy as np
+    import random
+    random.seed(seed)
+    np.random.seed(seed)
 
     env = gym.make(
         "LunarLander-v2",
@@ -32,18 +39,17 @@ def main(config_file):
         # render_mode = "rgb_array"
     )
 
-    to_render = True
-    seed = 0
 
-    env = MaxAndSkipEnv(env, frameskip=4, obs_len=4)
-    env = ScaleFrame(env, 84, 84, 4, interpolation=cv2.INTER_NEAREST)
-    # env = gym.make( "LunarLander-v2", continuous = False, gravity = -10.0, enable_wind = False, wind_power = 15.0, turbulence_power = 1.5)
+    # env = MaxAndSkipEnv(env, frameskip=4, obs_len=4)
+    # env = ScaleFrame(env, 84, 84, 4, interpolation=cv2.INTER_NEAREST)
+    env.seed(seed)
+    env.action_space.seed(seed)
 
     obs = env.reset()
     done = False
 
     print("obs shape", obs.shape, obs.min(), obs.max())
-    show_obs(obs, 4)
+    # show_obs(obs, 4)
     # plt.imshow(obs)
     # plt.show()
     i = 0
@@ -60,7 +66,8 @@ def main(config_file):
             done = term or trunc
 
         # print("done:", done)
-        print("obs here: ", new_obs.shape, new_obs.min(), new_obs.max())
+        print(f"act: {action} rew: {rew}")
+        # print("obs here: ", new_obs.shape, new_obs.min(), new_obs.max())
         if to_render:
             env.render(mode="human")
 
